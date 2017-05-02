347b6858a9e3cc55d9094bca68690ed81d4d44a6
blue <rong650751@163.com>
2017-05-02 23:56:55 +0800

    404页面

--- src/js/components/NotFound.js
+++ src/js/components/NotFound.js
@@ -6,9 +6,23 @@ export default class NotFound extends Component{
   }
   
   render() {
+    const styles = {
+      root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        background: 'url(\'./img/404.jpg\') no-repeat',
+        backgroundSize: 'cover',
+        color: 'gray',
+        fontSize: '28px'
+      }
+    }
+
     return (
-      <div>
-        404 not found
+      <div style={styles.root}>
+        面码不见啦～404
       </div>
     )
   }
